6f7979d9c197d15ff9cc3525b7819d8076d5d4b8
trong25 <he181249nguyenvantrong@gmail.com>
2025-05-13 21:18:27 +0700

    trong

--- SWP391_SE1932_GR4/src/java/Test.java
+++ SWP391_SE1932_GR4/src/java/Test.java
@@ -22,7 +22,7 @@ public class Test extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-      
+      String name = request.getParameter("name");
     } 
 
   
